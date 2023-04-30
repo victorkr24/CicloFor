4dc23f29028fffbaeb7ac8948adadfef20bc612c
VictorAviles124 <72232559+VictorAviles124@users.noreply.github.com>
2023-04-30 03:09:00 -0600

    Add project files.

---
+++ CicloFor/Controllers/CicloController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CicloFor.Controllers
+{
+    public class CicloController : Controller
+    {
+        public IActionResult Index()
+        {
+            var resultados = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                resultados.Add(i);
+            }
+
+            return View(resultados);
+        }
+    }
+}
